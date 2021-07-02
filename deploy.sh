dd46602bd4bb545b6c196f39fd6fb727ed7ec7a5
Hanmei <11611712@mail.sustech.edu.cn>
2021-07-02 20:40:04 +0800

    update the deploy.sh, synchronize two repos

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,16 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32mDeploying updates to GitHub, please enter the commit...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
+read msg
 git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
 git commit -m "$msg"
+git push
 
-# Push source and build repos.
+hugo
+
+cd public
+git add .
+git commit -m "$msg"
 git push
